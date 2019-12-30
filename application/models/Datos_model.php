0dd747829fe1a01d3ef4cc3d3ec25a5e551f53c3
uziel1111 <uziel.cap.123@gmail.com>
2019-12-30 17:03:51 -0600

    ajuste del query de la funcion get_datos_apa

--- application/models/Datos_model.php
+++ application/models/Datos_model.php
@@ -14,11 +14,35 @@ class Datos_model extends CI_Model
             ? AS periodo,
             cfg.nivel AS idnivel,
             ct.nombre AS encabezado_n_escuela,
-            m.nombre AS encabezado_muni_escuela
+            m.nombre AS encabezado_muni_escuela,
+            n.descr AS  encabezado_n_nivel,
+            t.descripcion AS encabezado_n_turno,
+            cfg.nivel,
+            CONCAT(p.nombre,' ',p.apell1,' ',p.apell2) AS encabezado_n_direc_resp,
+            est.alumnos1 AS asi_est_al_1,
+            est.alumnos2 AS asi_est_al_2,
+            est.alumnos3 AS asi_est_al_3,
+            est.alumnos4 AS asi_est_al_4,
+            est.alumnos5 AS asi_est_al_5,
+            est.alumnos6 AS asi_est_al_6,
+            est.t_alumnos AS asi_est_al_t,
+            est.grupos1 AS asi_est_gr_1,
+            est.grupos2 AS asi_est_gr_2,
+            est.grupos3 AS asi_est_gr_3,
+            est.grupos4 AS asi_est_gr_4,
+            est.grupos5 AS asi_est_gr_5,
+            est.grupos6 AS asi_est_gr_6,
+            est.t_grupos AS asi_est_gr_t,
+            est.t_docentes AS asi_est_do_t
+      
             FROM centrocfg cfg
             INNER JOIN cct ct ON ct.idct=cfg.idct
             INNER JOIN municipio m ON m.idmunicipio=ct.idmunicipio
-            where cfg.idcentrocfg= ?";
+            INNER JOIN niveleducativo n ON n.idnivel=cfg.nivel
+            INNER JOIN turno t ON t.idturno=cfg.turno
+            INNER JOIN personal p ON p.idcentrocfg=cfg.idcentrocfg AND idfuncion=1
+            LEFT JOIN estadistica_x_idcentrocfg est ON  est.`idcentrocfg`=cfg.`idcentrocfg`
+            WHERE cfg.idcentrocfg= ? ";
             // echo $q;die();
       return $this->db->query($q, array($periodo,$idcentrocfg))->row_array();
       // echo '<pre>';print_r($q);die();
