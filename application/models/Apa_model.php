9a302bba24f9e7e821eb690744cbb83594985e3a
uziel1111 <uziel.123@gmail.com>
2020-03-13 23:41:44 -0600

    correcion nuevo

--- application/models/Apa_model.php
+++ application/models/Apa_model.php
@@ -125,7 +125,7 @@ class Apa_model extends CI_Model
         //   if($x==6 || $x==22 || $x==26 || $x==28){
         //     $x=$x+1;
         //   }
-          if($x!=6 && $x!=22 && $x!=26 && $x!=28){
+          if($i!=6 && $i!=22 && $i!=26 && $i!=28){
 
         // $query=" INSERT INTO `apa_ok`.`planeaxidcentrocfg_reactivo` (
         //           `idcentrocfg`,
@@ -165,7 +165,7 @@ class Apa_model extends CI_Model
                        INNER JOIN planea_contenido c ON ud.id_unidad_analisis = c.id_unidad_analisis
                        INNER JOIN planea_reactivo r ON c.id_contenido = r.id_contenido
 
-                       WHERE cd.id_campodisiplinario=1
+                       WHERE cd.id_campodisiplinario=2
 
                        ORDER BY r.n_reactivo
                     ) aux on aux.n_reactivo = $i
@@ -175,6 +175,42 @@ class Apa_model extends CI_Model
            $this->db->query($query);
       }
     }
+//primaria español
+    for ($i=1; $i<51; $i++) {
+       //para español omitir reactivos 24,28,35,42,43
+       if($i!=2 && $i!=21 && $i!=30 && $i!=32 && $i!=48){
+
+       $query=" INSERT INTO `apa_ok`.`planeaxidcentrocfg_reactivo` (
+                 `idcentrocfg`,
+                 `id_reactivo`,
+                 `n_almn_eval`,
+                 `n_aciertos`,
+                 `id_periodo`
+               )
+               SELECT cfg.idcentrocfg, aux.id_reactivo AS id_reactivo,l.n_alum_eval,
+                 l.r{$i}_lyc,p.id_periodo
+                FROM `temporal_planea2` l
+                INNER JOIN cct ct ON ct.cct=l.cct AND ct.turno=l.turno
+                INNER JOIN centrocfg cfg ON cfg.idct=ct.idct AND l.turno=(IF(cfg.turno='M',100,(IF(cfg.turno='V',200,IF(cfg.turno='N',300,400)))))
+                INNER JOIN periodoplanea p ON p.periodo=l.periodo_planea
+                INNER JOIN (
+                  SELECT
+                   r.*
+                   FROM planea_camposdisciplinares cd
+                   INNER JOIN planea_unidad_analisis ud ON cd.id_campodisiplinario = ud.id_campodisiplinario AND ud.id_nivel=2
+                   INNER JOIN planea_contenido c ON ud.id_unidad_analisis = c.id_unidad_analisis
+                   INNER JOIN planea_reactivo r ON c.id_contenido = r.id_contenido
+
+                   WHERE cd.id_campodisiplinario=1
+
+                   ORDER BY r.n_reactivo
+                ) aux on aux.n_reactivo = $i
+                WHERE cfg.nivel=2
+             ";
+         // echo $query; die();
+         $this->db->query($query);
+       }
+     }
 
       //para primaria matemáticas mh
       for ($i=1; $i<51; $i++) {
@@ -212,72 +248,6 @@ class Apa_model extends CI_Model
         }
       }
 
-      $x=0;
-      for ($i=128; $i<173; $i++) {
-        //para español omitir reactivos
-         $x=$x+1;
-        //para español omitir los siguientes reactivos
-          if($x==2 || $x==21 || $x==30 || $x==32 || $x==48){
-            $x=$x+1;
-          }
-
-        $query=" INSERT INTO `apa_ok`.`planeaxidcentrocfg_reactivo` (
-                  `idcentrocfg`,
-                  `id_reactivo`,
-                  `n_almn_eval`,
-                  `n_aciertos`,
-                  `id_periodo`
-                )
-                SELECT cfg.idcentrocfg,'{$i}' AS id_reactivo,l.n_alum_eval,
-                  l.r{$x}_lyc,p.id_periodo
-                 FROM `temporal_planea2` l
-                 INNER JOIN cct ct ON ct.cct=l.cct AND ct.turno=l.turno
-                 INNER JOIN centrocfg cfg ON cfg.idct=ct.idct AND l.turno=(IF(cfg.turno='M',100,(IF(cfg.turno='V',200,IF(cfg.turno='N',300,400)))))
-                 INNER JOIN periodoplanea p ON p.periodo=l.periodo_planea
-                 WHERE cfg.nivel=2
-                 /*
-                 SELECT
-                  r.*
-                  FROM planea_camposdisciplinares cd
-                  INNER JOIN planea_unidad_analisis ud ON cd.id_campodisiplinario = ud.id_campodisiplinario AND ud.id_nivel=2
-                  INNER JOIN planea_contenido c ON ud.id_unidad_analisis = c.id_unidad_analisis
-                  INNER JOIN planea_reactivo r ON c.id_contenido = r.id_contenido
-
-                  WHERE cd.id_campodisiplinario=2
-
-                  ORDER BY r.n_reactivo
-                  */
-              ";
-           $this->db->query($query);
-      }
-
-      //para matematicas,primaria
-      $x=0;
-      for ($i=173; $i<221 ; $i++) {
-
-        $x=$x+1;
-        //para matematicas omitir los siguientes reactivos
-          if($x==2 || $x==26){
-            $x=$x+1;
-          }
-
-        $query=" INSERT INTO `apa_ok`.`planeaxidcentrocfg_reactivo` (
-                  `idcentrocfg`,
-                  `id_reactivo`,
-                  `n_almn_eval`,
-                  `n_aciertos`,
-                  `id_periodo`
-                )
-                SELECT cfg.idcentrocfg,'{$i}' AS id_reactivo,l.n_alum_eval,
-                  l.r{$x}_lyc,p.id_periodo
-                 FROM `temporal_planea2` l
-                 INNER JOIN cct ct ON ct.cct=l.cct AND ct.turno=l.turno
-                 INNER JOIN centrocfg cfg ON cfg.idct=ct.idct AND l.turno=(IF(cfg.turno='M',100,(IF(cfg.turno='V',200,IF(cfg.turno='N',300,400)))))
-                 INNER JOIN periodoplanea p ON p.periodo=l.periodo_planea
-                 WHERE cfg.nivel=2
-              ";
-           $this->db->query($query);
-      }
 
 
     }
